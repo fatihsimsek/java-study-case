76f6ca4a5078e2aeaf7e80c28acedf6ca6b11850
Fatih <fatih.simsek@akbank.com>
2021-11-05 10:45:05 +0300

    Add test classes

--- src/test/java/com/readingisgood/ordermanagement/CustomerTests.java
+++ src/test/java/com/readingisgood/ordermanagement/CustomerTests.java
@@ -1,11 +1,27 @@
 package com.readingisgood.ordermanagement;
 
+import com.readingisgood.ordermanagement.repository.CustomerRepository;
+import com.readingisgood.ordermanagement.service.CustomerService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 public class CustomerTests {
+    @MockBean
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private CustomerService customerService;
+
     @Test
-    void contextLoads() {
+    void createCustomer_Ok() {
+
+    }
+
+    @Test
+    void createCustomer_NotOk() {
+
     }
 }
--- src/test/java/com/readingisgood/ordermanagement/OrderTests.java
+++ src/test/java/com/readingisgood/ordermanagement/OrderTests.java
@@ -1,11 +1,27 @@
 package com.readingisgood.ordermanagement;
 
+import com.readingisgood.ordermanagement.repository.OrderRepository;
+import com.readingisgood.ordermanagement.service.OrderService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 public class OrderTests {
+    @MockBean
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private OrderService orderService;
+
     @Test
-    void contextLoads() {
+    void createOrder_Ok() {
+
+    }
+
+    @Test
+    void createOrder_NotOk() {
+
     }
 }
